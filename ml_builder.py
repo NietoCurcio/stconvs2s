ff917a9e73a506962499b5f3a0d0b0fa7b6a168e
Felipe Curcio <felipe_nieto010@hotmail.com>
2025-03-14 02:04:33 -0300

    update threshold to 0.001

--- ml_builder.py
+++ ml_builder.py
@@ -20,7 +20,7 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from torch import optim
 
-def clean_precipitation_data(data, property, threshold=0.0, verbose=True):
+def clean_precipitation_data(data, property, threshold=0.001, verbose=True):
     total_changes = 0
     max_changed_value = 0
     
